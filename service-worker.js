1f7074b568fdae58ee7570b1a9c792936e2c5852
Jimmy Praet <jimmy.praet@telenet.be>
2022-06-12 19:12:58 +0200

    Add Messages_kr.properties to app cache

--- service-worker.js
+++ service-worker.js
@@ -30,6 +30,7 @@ var URLS = [
   'i18n/Messages_fr.properties',
   'i18n/Messages_pl.properties',
   'i18n/Messages_ua.properties',
+  'i18n/Messages_kr.properties',
   'sound/clear.mp3',
   'sound/click.mp3',
   'sound/magic.mp3',
